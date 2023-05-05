d1d71f49ea95e21d3e0fae9ace0f08eb54a5c622
zengxian <xiangdong.zeng@intel.com>
2023-05-05 13:44:55 +0800

    for MiDaS

---
+++ launch_benchmark.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+set -xe
+
+# MiDaS
+function main {
+    # prepare workload
+    workload_dir="${PWD}"
+    # set common info
+    source oob-common/common.sh
+    init_params $@
+    fetch_device_info
+    set_environment
+
+    # requirements
+    if [ "${CKPT_DIR}" == "" ];then
+        set +x
+        echo "[ERROR] Please set CKPT_DIR before launch"
+        echo "  export CKPT_DIR=/path/to/checkpoint"
+        exit 1
+        set -x
+    fi
+
+    if [ ! -d rwightman_gen-efficientnet-pytorch_master ];then
+        git clone https://github.com/rwightman/gen-efficientnet-pytorch.git rwightman_gen-efficientnet-pytorch_master
+        cd rwightman_gen-efficientnet-pytorch_master
+        git checkout a4ac4dd7d72069a2aa4564df53ff4eb9b9f64400
+        patch -f -p1 < ${workload_dir}/container_abcs.patch || true
+        cd ..
+        cache_dir="${HOME}/.cache/torch/hub"
+        mkdir -p ${cache_dir} && rm -rf ${cache_dir}/rwightman_gen-efficientnet-pytorch_master
+        cp -r rwightman_gen-efficientnet-pytorch_master ${cache_dir}
+    fi
+
+    # if multiple use 'xxx,xxx,xxx'
+    model_name_list=($(echo "${model_name}" |sed 's/,/ /g'))
+    batch_size_list=($(echo "${batch_size}" |sed 's/,/ /g'))
+
+    # generate benchmark
+    for model_name in ${model_name_list[@]}
+    do
+        # pre run
+        python run.py --model_weights ${CKPT_DIR} --model_type small --arch midasnet \
+            --warmup_iterations 1 --num_iterations 2 \
+            --precision ${precision} --channels_last ${channels_last} || true
+        #
+        for batch_size in ${batch_size_list[@]}
+        do
+            # clean workspace
+            logs_path_clean
+            # generate launch script for multiple instance
+            if [ "${OOB_USE_LAUNCHER}" == "1" ] && [ "${device}" != "cuda" ];then
+                generate_core_launcher
+            else
+                generate_core
+            fi
+            # launch
+            echo -e "\n\n\n\n Running..."
+            cat ${excute_cmd_file} |column -t > ${excute_cmd_file}.tmp
+            mv ${excute_cmd_file}.tmp ${excute_cmd_file}
+            source ${excute_cmd_file}
+            echo -e "Finished.\n\n\n\n"
+            # collect launch result
+            collect_perf_logs
+        done
+    done
+}
+
+# run
+function generate_core {
+    # generate multiple instance script
+    for(( i=0; i<instance; i++ ))
+    do
+        real_cores_per_instance=$(echo ${cpu_array[i]} |awk -F, '{print NF}')
+        log_file="${log_dir}/rcpi${real_cores_per_instance}-ins${i}.log"
+
+        # instances
+        if [ "${device}" != "cuda" ];then
+            OOB_EXEC_HEADER=" numactl -m $(echo ${device_array[i]} |awk -F ';' '{print $2}') "
+            OOB_EXEC_HEADER+=" -C $(echo ${device_array[i]} |awk -F ';' '{print $1}') "
+        else
+            OOB_EXEC_HEADER=" CUDA_VISIBLE_DEVICES=${device_array[i]} "
+        fi
+
+        printf " ${OOB_EXEC_HEADER} \
+            python run.py --model_weights ${CKPT_DIR} --model_type small --arch midasnet \
+                --warmup_iterations ${num_warmup} --num_iterations ${num_iter} \
+                --precision ${precision} \
+                --channels_last ${channels_last} \
+                ${addtion_options} \
+        > ${log_file} 2>&1 &  \n" |tee -a ${excute_cmd_file}
+        if [ "${numa_nodes_use}" == "0" ];then
+            break
+        fi
+    done
+    echo -e "\n wait" >> ${excute_cmd_file}
+}
+
+# run
+function generate_core_launcher {
+    # generate multiple instance script
+    for(( i=0; i<instance; i++ ))
+    do
+        real_cores_per_instance=$(echo ${cpu_array[i]} |awk -F, '{print NF}')
+        log_file="${log_dir}/rcpi${real_cores_per_instance}-ins${i}.log"
+
+        printf "python -m oob-common.launch --enable_jemalloc \
+                    --core_list $(echo ${device_array[@]} |sed 's/;.//g') \
+                    --log_file_prefix rcpi${real_cores_per_instance} \
+                    --log_path ${log_dir} \
+                    --ninstances ${#cpu_array[@]} \
+                    --ncore_per_instance ${real_cores_per_instance} \
+            run.py --model_weights ${CKPT_DIR} --model_type small --arch midasnet \
+                --warmup_iterations ${num_warmup} --num_iterations ${num_iter} \
+                --precision ${precision} \
+                --channels_last ${channels_last} \
+                ${addtion_options} \
+        > /dev/null 2>&1 &  \n" |tee -a ${excute_cmd_file}
+        break
+    done
+    echo -e "\n wait" >> ${excute_cmd_file}
+}
+
+# download common files
+rm -rf oob-common && git clone https://github.com/intel-sandbox/oob-common.git
+
+# Start
+main "$@"
--- run.py
+++ run.py
@@ -6,6 +6,7 @@ import torch
 import utils
 import cv2
 import argparse
+import time
 
 from torchvision.transforms import Compose
 from midas.midas_net import MidasNet
@@ -13,7 +14,7 @@ from midas.midas_net_custom import MidasNet_small
 from midas.transforms import Resize, NormalizeImage, PrepareForNet
 
 
-def run(input_path, output_path, model_path, model_type="large", optimize=True):
+def run(input_path, output_path, model_path, model_type="large", jit=True):
     """Run MonoDepthNN to compute depth maps.
 
     Args:
@@ -24,7 +25,12 @@ def run(input_path, output_path, model_path, model_type="large", optimize=True):
     print("initialize")
 
     # select device
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if args.ipex:
+        import intel_extension_for_pytorch as ipex
+        print("using ipex model to do inference\n")
+        device = None
+    else:
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("device: %s" % device)
 
     # load network
@@ -32,7 +38,8 @@ def run(input_path, output_path, model_path, model_type="large", optimize=True):
         model = MidasNet(model_path, non_negative=True)
         net_w, net_h = 384, 384
     elif model_type == "small":
-        model = MidasNet_small(model_path, features=64, backbone="efficientnet_lite3", exportable=True, non_negative=True, blocks={'expand': True})
+        model = MidasNet_small(model_path, features=64, backbone="efficientnet_lite3",
+                exportable=True, non_negative=True, blocks={'expand': True})
         net_w, net_h = 256, 256
     else:
         print(f"model_type '{model_type}' not implemented, use: --model_type large")
@@ -55,16 +62,63 @@ def run(input_path, output_path, model_path, model_type="large", optimize=True):
     )
 
     model.eval()
-    
-    if optimize==True:
-        rand_example = torch.rand(1, 3, net_h, net_w)
+
+    if args.channels_last:
+        model_oob = model
+        model_oob = model_oob.to(memory_format=torch.channels_last)
+        model = model_oob
+        print('---- Use channels last format.')
+    else:
+        model.to(device)
+
+    if args.ipex:
+        if args.precision == "bfloat16":
+            model = ipex.optimize(model, dtype=torch.bfloat16, inplace=True)
+            print("running ipex bf16 evalation step\n")
+        else:
+            model = ipex.optimize(model, dtype=torch.float32, inplace=True)
+            print("running ipex fp32 evalation step\n")
+
+    if jit == True:
+        print("running jit fusion path\n")
+        rand_example = torch.rand(1, 3, net_h, net_w).to(device)
         model(rand_example)
         traced_script_module = torch.jit.trace(model, rand_example)
         model = traced_script_module
+        if args.ipex:
+            model = torch.jit.freeze(model)
     
         if device == torch.device("cuda"):
             model = model.to(memory_format=torch.channels_last)  
             model = model.half()
+
+        # warm-up
+        for i in range(10):
+            model(rand_example)
+
+    if args.trace:
+        from torch.fx import symbolic_trace
+        traced_dir = str(os.path.dirname(os.path.realpath(__file__))) + '/traced_model/'
+        if not os.path.exists(traced_dir):
+            os.makedirs(traced_dir)
+        traced_path = traced_dir + args.arch + "_fx_traced_model.pth"
+        # fx
+        try:
+            fx_traced = symbolic_trace(model)
+            torch.save(fx_traced, traced_path)
+        except:
+            print("WARN: {} don't support FX trace.".format(args.arch))
+        # jit
+        traced_path = traced_dir + args.arch + "_jit_traced_model.pth"
+        try:
+            q_model = torch.jit.script(model_.eval())
+            q_model.save(traced_path)
+        except:
+            try:
+                q_model = torch.jit.trace(model.eval(), images)
+                q_model.save(traced_path)
+            except:
+                print("WARN: {} don't support JIT script/trace.".format(args.arch))
 
     model.to(device)
 
@@ -77,50 +131,108 @@ def run(input_path, output_path, model_path, model_type="large", optimize=True):
 
     print("start processing")
 
+    all_time = 0
+    all_images = 0
+    batch_time_list = []
+    
     for ind, img_name in enumerate(img_names):
-
+        if args.num_iterations != 0 and ind > args.num_iterations:
+            break
         print("  processing {} ({}/{})".format(img_name, ind + 1, num_images))
-
         # input
-
         img = utils.read_image(img_name)
         img_input = transform({"image": img})["image"]
-
         # compute
         with torch.no_grad():
             sample = torch.from_numpy(img_input).to(device).unsqueeze(0)
-            if optimize==True and device == torch.device("cuda"):
-                sample = sample.to(memory_format=torch.channels_last)  
-                sample = sample.half()
-            prediction = model.forward(sample)
+            if args.channels_last:
+                sample = sample.to(memory_format=torch.channels_last)
+            start = time.time()
+            if args.profile:
+                with torch.profiler.profile(activities=[torch.profiler.ProfilerActivity.CPU], record_shapes=True) as prof:
+                    prediction = model.forward(sample)
+                #
+                if ind == int(args.num_iterations/2):
+                    import pathlib
+                    timeline_dir = str(pathlib.Path.cwd()) + '/timeline/'
+                    if not os.path.exists(timeline_dir):
+                        os.makedirs(timeline_dir)
+                    timeline_file = timeline_dir + 'timeline-' + str(torch.backends.quantized.engine) + '-' + \
+                                args.arch + str(ind) + '-' + str(os.getpid()) + '.json'
+                    print(timeline_file)
+                    prof.export_chrome_trace(timeline_file)
+                    table_res = prof.key_averages().table(sort_by="cpu_time_total")
+                    print(table_res)
+                    # self.save_profile_result(timeline_dir + torch.backends.quantized.engine + "_result_average.xlsx", table_res)
+            else:
+                prediction = model.forward(sample)
+            #prediction = model.forward(sample)
             prediction = (
                 torch.nn.functional.interpolate(
                     prediction.unsqueeze(1),
-                    size=img.shape[:2],
+                    size=img.shape[:2], 
                     mode="bicubic",
                     align_corners=False,
                 )
                 .squeeze()
-                .cpu()
-                .numpy()
+                .cpu().float().numpy()
             )
+            end = time.time()
+            print("Iteration: {}, inference time: {} sec.".format(ind, end - start), flush=True)
+            if ind >= args.warmup_iterations:
+                batch_time_list.append((end - start) * 1000)
+                all_time += end - start
+                all_images += 1
 
-        # output
-        filename = os.path.join(
-            output_path, os.path.splitext(os.path.basename(img_name))[0]
-        )
-        utils.write_depth(filename, prediction, bits=2)
+            # output
+            filename = os.path.join(
+                output_path, os.path.splitext(os.path.basename(img_name))[0]
+            )
+            utils.write_depth(filename, prediction, bits=2)
+
+    print("\n", "-"*20, "Summary", "-"*20)
+    latency = all_time / all_images * 1000
+    throughput = all_images / all_time
+    print("Latency:\t {:.3f} ms".format(latency))
+    print("Throughput:\t {:.2f} samples/s".format(throughput))
+    # P50
+    batch_time_list.sort()
+    p50_latency = batch_time_list[int(len(batch_time_list) * 0.50) - 1]
+    p90_latency = batch_time_list[int(len(batch_time_list) * 0.90) - 1]
+    p99_latency = batch_time_list[int(len(batch_time_list) * 0.99) - 1]
+    print('Latency P50:\t %.3f ms\nLatency P90:\t %.3f ms\nLatency P99:\t %.3f ms\n'\
+            % (p50_latency, p90_latency, p99_latency))
 
     print("finished")
+
+
+def save_profile_result(filename, table):
+    import xlsxwriter
+    workbook = xlsxwriter.Workbook(filename)
+    worksheet = workbook.add_worksheet()
+    keys = ["Name", "Self CPU total %", "Self CPU total", "CPU total %" , "CPU total", \
+            "CPU time avg", "Number of Calls"]
+    for j in range(len(keys)):
+        worksheet.write(0, j, keys[j])
+
+    lines = table.split("\n")
+    for i in range(3, len(lines)-4):
+        words = lines[i].split(" ")
+        j = 0
+        for word in words:
+            if not word == "":
+                worksheet.write(i-2, j, word)
+                j += 1
+    workbook.close()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-i', '--input_path', 
-        default='input',
-        help='folder with input images'
-    )
+    parser.add_argument('-i', '--input_path',
+                        default='input',
+                        help='folder with input images'
+                        )
 
     parser.add_argument('-o', '--output_path', 
         default='output',
@@ -137,9 +249,20 @@ if __name__ == "__main__":
         help='model type: large or small'
     )
 
-    parser.add_argument('--optimize', dest='optimize', action='store_true')
+    parser.add_argument('--jit', dest='jit', action='store_true')
     parser.add_argument('--no-optimize', dest='optimize', action='store_false')
-    parser.set_defaults(optimize=True)
+    parser.add_argument('--profile', action='store_true',help='help')
+    parser.add_argument('--trace', action='store_true',help='help')
+    parser.add_argument('--arch', type=str, help='model name')
+    parser.add_argument('--ipex', action='store_true', default=False,
+                        help='use intel pytorch extension')
+    parser.add_argument('--precision', type=str, default="float32",
+                        help='precision, float32, bfloat16')
+    parser.add_argument('-w', '--warmup_iterations', default=5, type=int, metavar='N',
+                        help='number of warmup iterations to run')
+    parser.add_argument('--num_iterations', default=0, type=int, metavar='N',
+                        help='number of iterations to run')
+    parser.add_argument('--channels_last', type=int, default=1, help='use channels last format')
 
     args = parser.parse_args()
 
@@ -148,4 +271,8 @@ if __name__ == "__main__":
     torch.backends.cudnn.benchmark = True
 
     # compute depth maps
-    run(args.input_path, args.output_path, args.model_weights, args.model_type, args.optimize)
+    if args.precision == "bfloat16":
+        with torch.cpu.amp.autocast(enabled=True, dtype=torch.bfloat16):
+            run(args.input_path, args.output_path, args.model_weights, args.model_type, args.jit)
+    else:
+        run(args.input_path, args.output_path, args.model_weights, args.model_type, args.jit)
